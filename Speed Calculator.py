061fe535edf1cb38aa958ff2e98aadf59b085e45
jaimanral <jaimanral@ymail.com>
2018-04-03 13:48:34 +0530

    Add files via upload

---
+++ Speed Calculator.py
@@ -0,0 +1,8 @@
+uphillDistance = int (input ())
+downhillDistance = int (input ())
+uphillTime = int (input ())
+downhillTime  = int (input ())
+totalDistance = uphillDistance + downhillDistance
+totalTime = uphillTime + downhillTime
+averageSpeed = totalDistance /totalTime
+print(averageSpeed)
